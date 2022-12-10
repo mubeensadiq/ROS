1a7fe9a09f96e4a88acb9ab1a4d503a8648af6bd
mubeensadiq <mubeensadiq95@gmail.com>
2022-12-10 19:42:23 +0000

    migrations

---
+++ database/migrations/2022_12_10_184500_create_areas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')
+                ->constrained('cities')
+                ->onDelete('cascade');
+            $table->string('area');
+            $table->string('postal_code');
+            $table->string('delivery_time');
+            $table->double('delivery_charges');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+};
---
+++ database/migrations/2022_12_10_191013_create_rider_profile_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rider_profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->foreignId('area_id')
+                ->nullable()
+                ->constrained('areas')
+                ->nullOnDelete();
+            $table->string('cnic');
+            $table->string('license_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rider_profile');
+    }
+};
