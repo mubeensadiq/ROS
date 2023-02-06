d30d27c37e9ec87115700bc20356126a4a3aeeb8
mubeensadiq <mubeensadiq95@gmail.com>
2023-02-06 20:51:49 +0000

    users fixes

--- resources/js/pages/AddUser.vue
+++ resources/js/pages/AddUser.vue
@@ -50,15 +50,14 @@ export default {
                 this.getUserDetails(this.$route.params.id);
             });
         }
-        else{
-        }
     },
     watch: {},
     methods: {
         getRoles() {
             axios.get('/api/roles').then((response) => {
                 this.roles = response.data.roles;
-                this.user.role = this.roles[0].id.toString();
+                if(!this.update)
+                    this.user.role = this.roles[0].id.toString();
             }).catch((error) => {
                 this.showNoty(error.response.data.message, 'error')
             });
