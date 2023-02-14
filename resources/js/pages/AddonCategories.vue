cd51a7cf2f212088b32acd70f60c426059aaf643
mubeensadiq <mubeensadiq95@gmail.com>
2023-02-14 17:26:10 +0000

    addon categories

--- resources/js/pages/AddonCategories.vue
+++ resources/js/pages/AddonCategories.vue
@@ -134,7 +134,7 @@ export default {
                             class="first:rounded-l-md last:rounded-r-md w-56 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-0 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400"
                         >
                             <div class="flex items-center justify-center">
-                                <RouterLink v-if="$can('categories.update')" :to="{name : 'categories.update', params:{'id' : category.id} }"
+                                <RouterLink v-if="$can('addon.categories.update')" :to="{name : 'addon.categories.update', params:{'id' : category.id} }"
                                             class="flex items-center mr-3">
                                     <Lucide icon="CheckSquare" class="w-4 h-4 mr-1"/>
                                     Edit
