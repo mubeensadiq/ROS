7b5790677c8d57c82afc7b282a3898ae10257f32
mubeensadiq <mubeensadiq95@gmail.com>
2023-03-07 16:38:29 +0000

    delete addon

--- resources/js/pages/Addons.vue
+++ resources/js/pages/Addons.vue
@@ -9,10 +9,7 @@ import Lucide from "../base-components/Lucide";
 import Tippy from "../base-components/Tippy";
 import { Dialog, Menu } from "../base-components/Headless";
 import Table from "../base-components/Table";
-const deleteConfirmationModal = ref(false);
-const setDeleteConfirmationModal = (value: boolean) => {
-    deleteConfirmationModal.value = value;
-};
+
 const deleteButtonRef = ref(null);
 const limits = pageLimits();
 import Notification from "./Notification.vue";
